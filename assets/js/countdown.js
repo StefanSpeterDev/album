ec5a0009af784962fb9e9fc113389d35e26af9e7
StefanYYC <stefanspeter26@gmail.com>
2021-12-14 16:39:39 +0100

    page timer faite

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,74 +1,45 @@
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
-var Clock = (function(){
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy + 1,
+    dayMonth = "09/30/",
+    birthday = dayMonth + yyyy;
 
-	var exports = function(element) {
-		this._element = element;
-		var html = '';
-		for (var i=0;i<6;i++) {
-			html += '<span>&nbsp;</span>';
-		}
-		this._element.innerHTML = html;
-		this._slots = this._element.getElementsByTagName('span');
-		this._tick();
-	};
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
 
-	exports.prototype = {
+  const countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
 
-		_tick:function() {
-			var time = new Date();
-			this._update(this._pad(time.getHours()) + this._pad(time.getMinutes()) + this._pad(time.getSeconds()));
-			var self = this;
-			setTimeout(function(){
-				self._tick();
-			},1000);
-		},
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
 
-		_pad:function(value) {
-			return ('0' + value).slice(-2);
-		},
-
-		_update:function(timeString) {
-
-			var i=0,l=this._slots.length,value,slot,now;
-			for (;i<l;i++) {
-
-				value = timeString.charAt(i);
-				slot = this._slots[i];
-				now = slot.dataset.now;
-
-				if (!now) {
-					slot.dataset.now = value;
-					slot.dataset.old = value;
-					continue;
-				}
-
-				if (now !== value) {
-					this._flip(slot,value);
-				}
-			}
-		},
-
-		_flip:function(slot,value) {
-
-			// setup new state
-			slot.classList.remove('flip');
-			slot.dataset.old = slot.dataset.now;
-			slot.dataset.now = value;
-
-			// force dom reflow
-			slot.offsetLeft;
-
-			// start flippin
-			slot.classList.add('flip');
-
-		}
-
-	};
-
-	return exports;
-}());
-
-var i=0,clocks = document.querySelectorAll('.clock'),l=clocks.length;
-for (;i<l;i++) {
-	new Clock(clocks[i]);
-}
+      //do something later when date is reached
+      if (distance < 0) {
+        clearInterval(x);
+      }
+      //seconds
+    }, 0);
+})();
