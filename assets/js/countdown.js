27b07a7ec3457987601f9d50de47949c7b8095f0
StefanYYC <stefanspeter26@gmail.com>
2021-12-17 10:41:39 +0100

    histoire fusion dans countdown

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -17,6 +17,7 @@ function startTimer(duration, houres, min, sec) {
     if (--timer < 0) {
       clearInterval(x);
       $(".wrapper-time").css("animation", "disappear 3s  forwards");
+      $(".timer").css("animation", "disappear 3s  forwards");
       $("#button-disappear").css("animation", "disappear 3s  forwards");
       $(".zoomoutheader").css("animation", "disappear 3s forwards");
 
@@ -24,23 +25,30 @@ function startTimer(duration, houres, min, sec) {
       moon.css({
         "background-size": "cover",
         "height": "100%",
-        "animation": "moonup 3s ease-in forwards",
+        "animation": "moonup 6s ease-in forwards",
         "overflow": "visible",
         "margin-top": "-250px"
       });
       moon
         .css(
           "background-image",
-          'url("' + "../assets/images/background_long_final.png" + '")'
+          'url("' + "../assets/images/background.jpg" + '")'
         ) //Change BG
         .fadeIn(); //FadeIn
       $(".header-wrapper .block-rs").css("top", "30%");
+
+      setTimeout(function(){
+        $('.wrapper-post-timer').fadeIn('slow');
+        $('section.histoire').fadeIn('slow');
+    },5500);
     }
   }, 1000);
 }
 
 jQuery(function ($) {
-  var fiveMinutes = 60 * 0.03;
+  var fiveMinutes = 60 * 0.06;
+  $(".wrapper-post-timer").hide();
+  $("section.histoire").hide();
   min = $("#minutes");
   hours = $("#hours");
   sec = $("#seconds");
